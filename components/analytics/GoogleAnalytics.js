534fc5a8063ca280acc00b080d34705135ce00d1
chriselias <chris@chriselias.com>
2022-05-09 11:57:06 -0400

    trying to fix GA script tag again

--- components/analytics/GoogleAnalytics.js
+++ components/analytics/GoogleAnalytics.js
@@ -4,14 +4,22 @@ import siteMetadata from '@/data/siteMetadata'
 
 const GAScript = () => {
   return (
-    <><script async src="https://www.googletagmanager.com/gtag/js?id=G-6DCSDW1PQS"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-6DCSDW1PQS');
-    </script>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
+      />
+
+      <Script strategy="lazyOnload" id="ga-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
     </>
   )
 }
