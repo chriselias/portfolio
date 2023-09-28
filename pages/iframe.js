834976e721da12d2623c2627db256b30a25da91d
chriselias <chris@chriselias.com>
2023-09-28 15:58:36 -0400

    iframe test

---
+++ pages/iframe.js
@@ -0,0 +1,21 @@
+export default function Page() {
+  return (
+    <>
+      <h1>iframe test</h1>
+      <iframe
+        data-app-id="app_575220f8-f6dd-4577-97c9-80a3918d1a10"
+        allow=""
+        aria-hidden="false"
+        sandbox="allow-scripts allow-forms allow-same-origin allow-modals allow-presentation allow-popups allow-downloads"
+        src="https://webui-fe-5767.staging.capacity.com/"
+        name="app-instance-ember1162"
+        id="ember3093"
+        className="app-embed external-app app-575220f8-f6dd-4577-97c9-80a3918d1a10 ember-view visible"
+        width={1000}
+        height={1000}
+      >
+        <p>Your browser does not support iframes.</p>
+      </iframe>
+    </>
+  )
+}
